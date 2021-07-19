13444bb97cf831a9d135ab3984bf291b4c343b72
nimishathekkarath <87378057+nimishathekkarath@users.noreply.github.com>
2021-07-19 18:13:28 +0100

    Update super_simple_stocks.py

--- super_simple_stocks.py
+++ super_simple_stocks.py
@@ -1,12 +1,8 @@
-"""
-File:  super_simple_stockes.py
-Created By:  Nimisha Thekkarath(nimisha0092@gmail.com)
-"""
 import enum
+import abc
 import operator
 import logging
 
-from abc import ABC, abstractmethod
 from datetime import datetime, timedelta
 from functools import reduce
 
@@ -15,120 +11,132 @@ logging.basicConfig(filename='super_simple_stockers.log',
                     level = logging.DEBUG,
                     format = LOG_FORMAT,
                     filemode = 'w' )
-logger = logging.getLogger()                    
+logger = logging.getLogger() 
 
 @enum.unique
-class StockSymbol(enum.Enum):
-    """unique isdentifier for each stocks"""
-    TEA=1
-    POP=2
-    ALE=3
-    GIN=4
-    JOE=5
+class TickerSymbol(enum.Enum):
+
+    """Unique identifier for one of the traded stocks"""
+
+    TEA = 1
+    POP = 2
+    ALE = 3
+    GIN = 4
+    JOE = 5
+
 
 @enum.unique
-class BuyOrSell(enum.Enum):
-    """Indicator to represent buying or selling the trade"""
-    BUY=1
-    SELL=2
+class BuySellIndicator(enum.Enum):
+
+    """Indicator to buy or sell that accompanies each trade"""
+
+    BUY = 1
+    SELL = 2
+
 
 class Trade:
-    """The Trade class will collect all the details retaled to the Trade"""
+
+    """A change of ownership of a collection of shares at a definite price per share"""
+
     def __init__(self,
-                 stock_symbol: StockSymbol,
+                 ticker_symbol: TickerSymbol,
                  timestamp: datetime,
                  quantity: int,
                  price_per_share: float,
-                 buyorsell: BuyOrSell):
-                 
+                 buy_sell_indicator: BuySellIndicator):
         """
-        :parram stock_symbol: TEA,POP,ALE,GIN or JOE
-        :parram timestamp: The moment when the transaction has taken place
-        :parram quantity: Amount of shares 
-        :parram price_per_share: Price for each share
-        :parram buyorsell: Indication to buy or sell
-        """     
-        logger.info("Creating new Trade")
+        :param timestamp: The moment when the transaction has taken place
+        :param quantity: The amount of shares exchanged
+        :param price_per_share: Price for each share
+        :param buy_sell_indicator: Indication to buy or sell
+        """
+       
+        self.ticker_symbol = ticker_symbol
+        self.timestamp = timestamp
+        logger.info("Creating new Trade={}".format(self.ticker_symbol))
         if quantity > 0:
             self.quantity = quantity
         else:
-            logger.error("quantity of the share should be positive")
+            msg = "The quantity of shares has to be positive."
+            raise ValueError(msg)
 
-        if  price_per_share >= 0:
+        if price_per_share >= 0.0:
             self.price_per_share = price_per_share
         else:
-            logger.error("price per share should be positive") 
+            msg = "The price per share can not be negative."
+            raise ValueError(msg)
 
-        self.stock_symbol = stock_symbol
-        self.buyorsell = buyorsell 
-        self.timestamp = timestamp
+        self.buy_sell_indicator = buy_sell_indicator
         logger.info("Created Trade: total_price ={}".format(self.total_price))
 
     @property
     def total_price(self) -> float:
         """
-        :return: Total price of the trade
+        :return: The total price of the trade
         """
         return self.quantity * self.price_per_share
 
-class Stock(ABC):
-    """
-    This is an abstract class for CommonStock and PrefferedStock.
-    It has an abstract method 'dividend'.
-    It has common interfaces that both common and preferred stocks share.
 
-    ..note:: The class variable Stock.price_time_interval is the length of the time interval that is significant to calculate the stock
+class Stock(abc.ABC):
+
+    """A publicly traded stock
+
+    This is an abstract class that includes the common interface that both common
+    and preferred stocks share.
+
+    .. note:: The class variable Stock.price_time_interval serves as a configuration value to
+        define the length of the time interval that is significant to calculate the stock
         price.
-    """ 
+    """
 
-    price_time_interval = timedelta(minutes=15)   
-    
+    price_time_interval = timedelta(minutes=15)
+
     def __init__(self,
-                stock_symbol:StockSymbol,
-                par_value:float):
+                 ticker_symbol: TickerSymbol,
+                 par_value: float):
         """
-        :parram stock_symbol: The stock_symbol that identifies this stock(TEA,POP,ALE,GIN or JOE )
-        :param par_value: The par value per share for this stock
-
+        :param ticker_symbol: The ticker_symbol that identifies this stock
+        :param par_value: The face value per share for this stock
         .. note:: This initializer also creates the instance variable self.trades,
             which is to hold a list of recorded instances of Trade.
         """
         logger.info("Creating new Trade")
-        self.stock_symbol = stock_symbol
+        self.ticker_symbol = ticker_symbol
         self.par_value = par_value
-
+        logger.info("Created new Trade={}".format(self.ticker_symbol))
         self.trades = []
 
     def record_trade(self, trade: Trade):
         """Records a trade for this stock.
         :param trade: The trade to be recorded
-        :raise TypeError: If the instance is not Trade the raise TypeError
-        :raise ValueError: Checks the stock_symbol of the trade 
+        :raise TypeError:
+        :raise ValueError:
         """
         logger.info("Recording a trade")
-
         if not isinstance(trade, Trade):
-            logger.error("Argument trade={trade} should be of type Trade.".format(trade=trade))
-        elif self.stock_symbol is not trade.stock_symbol:
-            logger.error("Argument trade={trade} does not belong to this stock.".format(trade=trade))
+            msg = "Argument trade={trade} should be of type Trade.".format(trade=trade)
+            raise TypeError(msg)
+        elif self.ticker_symbol is not trade.ticker_symbol:
+            msg = "Argument trade={trade} does not belong to this stock.".format(trade=trade)
+            raise ValueError(msg)
         else:
-            self.trades.append(trade)    
+            self.trades.append(trade)
 
     @property
-    @abstractmethod
-    def dividend(self)-> float:
+    @abc.abstractmethod
+    def dividend(self) -> float:
         """
-        :retuen: The dividend for the given stock
+        :return: A ratio that represents the dividend for this stock
         """
         pass
 
     @property
-    def stock_price(self) -> float:
-        """self.trades
+    def ticker_price(self) -> float:
+        """
         :return: The price per share for the last recorded trade for this stock
         :raise AttributeError:
-        .. note:: First we will sort the trade according to the timestamp,
-           then we will take the latest price of the stock.
+        .. note:: We don't know if the trades will be registered in chronological order.
+            That is why self.trades is explicitly sorted.
         """
         logger.info("Accessing stock_price")
         if len(self.trades) > 0:
@@ -137,7 +145,8 @@ class Stock(ABC):
                                   reverse=True)
             return by_timestamp[0].price_per_share
         else:
-            logger.error("The last stock price is not yet available.")
+            msg = "The last ticker price is not yet available."
+            raise AttributeError(msg)
 
     @property
     def dividend_yield(self) -> float:
@@ -147,6 +156,7 @@ class Stock(ABC):
             return dividendyield
         except ZeroDivisionError:
             logger.critical("ZeroDivisionError occured", exc_info=True)
+        
 
     @property
     def price_earnings_ratio(self) -> float:
@@ -155,25 +165,27 @@ class Stock(ABC):
         """
         logger.info("Calculating price_earnings_ratio")
         if self.dividend != 0:
-            return self.stock_price / self.dividend
+            return self.ticker_price / self.dividend
         else:
-            return None    
+            return None
 
     def price(self,
               current_time: datetime=datetime.now()) -> float:
         """
-        :param current_time: The Current time.
+        :param current_time: The point of time defined as the current one.
         :return: The average price per share based on trades recorded in the last
             Stock.price_time_interval. None if there are 0 trades that satisfy this
             condition.
-        
+        .. note:: Though lean, the way in which significant_trades obtained may be
+            unnecessarily costly, since it traverses all recorded trades and it may
+            be possible to have them already ordered by trade.timestamp.
+        .. note:: The existence of the current_time parameter avoids the inner user
+            of datetime.now, thus keeping referential transparency and moving state out.
         """
-        """collect the trades that happend in this perticular span of time in to significant_trades""" 
-
         logger.info("Calculating price")
         significant_trades = [trade for trade in self.trades
                               if trade.timestamp >= current_time - self.price_time_interval]
-
+        
         if len(significant_trades) > 0:
             trade_prices = (trade.total_price for trade in significant_trades)
             quantities = (trade.quantity for trade in significant_trades)
@@ -184,52 +196,58 @@ class Stock(ABC):
                 logger.error("ZeroDivisionError", exc_info=True)
         else:
             return None
-  
+       
+
 class CommonStock(Stock):
-    """Common Stock"""
+
+    """A common stock"""
 
     def __init__(self,
-                stock_symbol:StockSymbol,
-                par_value:float,
-                last_dividend: float):
+                 ticker_symbol: TickerSymbol,
+                 par_value: float,
+                 last_dividend: float):
         """
-        :param last_dividend: Last dividend of the given stock 
-        """  
+        :param last_dividend: An absolute value that indicates the last dividend
+            per share for this stock.
+        """
         logger.info("Common Stock") 
-        super().__init__(stock_symbol, par_value)
-        self.last_dividend = last_dividend 
+        super().__init__(ticker_symbol, par_value)
+        self.last_dividend = last_dividend
 
     @property
     def dividend(self):
-        logger.info("Calculate dividend for a Common Stock") 
+        logger.info("Calculate dividend for a Common Stock")
         return self.last_dividend
 
+
 class PreferredStock(Stock):
-    """Preferred Stock"""
+
+    """A preferred stock"""
 
     def __init__(self,
-                stock_symbol: StockSymbol,
-                par_value: float,
-                fixed_dividend: float):
+                 ticker_symbol: TickerSymbol,
+                 par_value: float,
+                 fixed_dividend: float):
         """
         :param fixed_dividend: A decimal number that expresses the fixed dividend
             as a ratio of the face value of each share.
         """
         logger.info("Preferred Stock") 
-        super().__init__(stock_symbol, par_value)
+        super().__init__(ticker_symbol, par_value)
         self.fixed_dividend = fixed_dividend
 
     @property
     def dividend(self):
         logger.info("Calculate dividend for a Preferred Stock") 
-        return self.fixed_dividend * self.par_value  
+        return self.fixed_dividend * self.par_value
+
 
 class GlobalBeverageCorporationExchange:
 
     """The whole exchange where the trades take place"""
 
     def __init__(self,
-                  stocks: list[Stock] ):
+                 stocks: list[Stock]):
         """
         :param stocks: The stocks traded at this exchange.
         :raise ValueError:
@@ -238,7 +256,8 @@ class GlobalBeverageCorporationExchange:
         if len(stocks) > 0:
             self.stocks = stocks
         else:
-            logger.error("Argument stocks={stocks} should be a non empty sequence.".format(stocks=stocks))
+            msg = "Argument stocks={stocks} should be a non empty sequence.".format(stocks=stocks)
+            raise ValueError(msg)
 
     def record_trade(self,
                      trade: Trade):
@@ -247,7 +266,7 @@ class GlobalBeverageCorporationExchange:
         """
         logger.info("Records a trade for the proper stock")
         stock = next(stock for stock in self.stocks
-                     if stock.stock_symbol is trade.stock_symbol)
+                     if stock.ticker_symbol is trade.ticker_symbol)
         stock.record_trade(trade)
 
     def geometric_mean(self,
@@ -257,24 +276,13 @@ class GlobalBeverageCorporationExchange:
         :return: The geometric mean of all stock prices. Returns None if any of them is
             None.
         """
-         
         logger.info("Finding The geometric mean of all stock prices")
         n = len(self.stocks)
-        if n==0:
-            logger.error("quantity of the share should be positive")
-
         stock_prices = [stock.price(current_time) for stock in self.stocks]
 
         if None in stock_prices:
             return None
         else:
             product = reduce(operator.mul, stock_prices, 1)
-            geometricmean=product**(1/n)
-            return geometricmean
-            
-    
-   
-
-
-
+            return product**(1/n)
 
