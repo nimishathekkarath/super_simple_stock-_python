3adefe7953bdaf3e753d11ed88a705543f9fbb4a
nimishathekkarath <87378057+nimishathekkarath@users.noreply.github.com>
2021-07-19 18:11:44 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Super Simple Stocks",
+    packages=find_packages(),
+    version="0.1.0",
+    description="Assignment for the hiring process for a position at J.P.Morgan",
+    author="Nimisha Thekkarath",
+    author_email="nimisha0092@gmail.com",
+    url="https://github.com/nimishathekkarath/super_simple_stocks.git",
+    long_description=open('README.md').read()
+)
