93ad88f6af12e7186e5aef0ac442f6f9e5bff7cc
nimishathekkarath <87378057+nimishathekkarath@users.noreply.github.com>
2021-07-19 12:23:37 +0100

    Add files via upload

---
+++ tests/test_global_beverage_corporation_exchange.py
@@ -0,0 +1,47 @@
+import unittest
+
+from super_simple_stocks import GlobalBeverageCorporationExchange, StockSymbol
+from .factories import StockFactory, TradeFactory
+
+
+class GlobalBeverageCorporationExchangeInitTestCase(unittest.TestCase):
+
+    def test_checks_empty_stocks(self):
+        with self.assertRaises(ValueError):
+            gbce = GlobalBeverageCorporationExchange([])
+
+
+class GlobalBeverageCorporationExchangeRecordAllShareIndexTestCase(unittest.TestCase):
+
+    def test_not_enough_significant_trades_returns_none(self):
+        stocks = StockFactory.get_stocks()
+        gbce = GlobalBeverageCorporationExchange(stocks)
+        index = gbce.geometric_mean()
+        self.assertIsNone(index)
+
+    def test_index_value(self):
+
+        tea_stock = StockFactory.get_stock_by_stock_symbol(StockSymbol.TEA)
+        gin_stock = StockFactory.get_stock_by_stock_symbol(StockSymbol.GIN)
+        gbce = GlobalBeverageCorporationExchange([tea_stock, gin_stock])
+
+        tea_stock_trades = TradeFactory.get_trades_for_stock(StockSymbol.TEA)
+        gin_stock_trades = TradeFactory.get_trades_for_stock(StockSymbol.GIN)
+
+        for trade in tea_stock_trades + gin_stock_trades:
+            gbce.record_trade(trade)
+
+        last_tea_stock_trade = sorted(tea_stock_trades,
+                                      key=lambda t: t.timestamp,
+                                      reverse=True)[0]
+        last_gin_stock_trade = sorted(gin_stock_trades,
+                                      key=lambda t: t.timestamp,
+                                      reverse=True)[0]
+
+        current_time = max([last_tea_stock_trade.timestamp,
+                            last_gin_stock_trade.timestamp])
+        tea_stock_price = tea_stock.price(current_time)
+        gin_stock_price = gin_stock.price(current_time)
+        expected_value = (tea_stock_price * gin_stock_price)**(1/2)
+
+        self.assertEqual(gbce.geometric_mean(current_time), expected_value)
